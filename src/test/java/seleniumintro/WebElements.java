affa122b2210dcd45748886a7fbd85462773ad84
AntonSemenov87 <Anthony04061987@gmail.com>
2020-09-28 22:44:05 -0500

    additions

---
+++ src/test/java/seleniumintro/WebElements.java
@@ -0,0 +1,21 @@
+package seleniumintro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebElements {
+    public static void main(String[] args) throws Exception {
+
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.google.com");
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("unifi dream machine" + Keys.ENTER);
+
+
+    }
+}
