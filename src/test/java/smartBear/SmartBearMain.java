20020ce04ae3c52b48856c568078020948cd6909
AntonSemenov87 <Anthony04061987@gmail.com>
2020-10-07 19:32:51 -0500

    smartbear tests

--- src/test/java/smartBear/SmartBearMain.java
+++ src/test/java/smartBear/SmartBearMain.java
@@ -3,6 +3,8 @@ package smartBear;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.yaml.snakeyaml.error.Mark;
 
 import java.util.List;
 
@@ -32,6 +34,20 @@ public class SmartBearMain {
 
         for (int i = 0; i < allNames.size(); i++) {
             System.out.println(allNames.get(i).getText() + " <--name, city --> " + allCities.get(i).getText());
+        }
+    }
+
+    public static void deleteName (WebDriver driver, String nameToBeDeleted) {
+        String dynamicCheckboxForUserLocatorXpath = "//table[@id='ctl00_MainContent_orderGrid']/tbody/tr/td[.='" + nameToBeDeleted + "']/../td[1]";
+        driver.findElement(By.xpath(dynamicCheckboxForUserLocatorXpath)).click();
+
+        String deleteSelectedBtnLocatorID = "ctl00_MainContent_btnDelete";
+        driver.findElement(By.id(deleteSelectedBtnLocatorID)).click();
+
+        List<WebElement> allNames = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']/tbody/tr/td[2]"));
+
+        for (WebElement name: allNames) {
+            Assert.assertFalse(name.getText().equals(nameToBeDeleted), "This TC failed, it has name in the list");
         }
     }
 
--- src/test/java/testng/SmartBear_deleteOrder_editOrder.java
+++ src/test/java/testng/SmartBear_deleteOrder_editOrder.java
@@ -6,14 +6,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import smartBear.SmartBearMain;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class SmartBear_deleteOrder {
+public class SmartBear_deleteOrder_editOrder {
 
     WebDriver driver;
 
@@ -21,13 +23,17 @@ public class SmartBear_deleteOrder {
     public void setUp (){
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
+    }
+
+    @BeforeMethod
+    public void landOnHomePageAndLogin(){
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        SmartBearMain.login(driver);
     }
 
     @Test
     public void deleteOrder() {
-        SmartBearMain.login(driver);
         String checkboxForMarkSmithLocatorXpath = "//table[@id='ctl00_MainContent_orderGrid']/tbody/tr/td[.='Mark Smith']/../td[1]";
         driver.findElement(By.xpath(checkboxForMarkSmithLocatorXpath)).click();
 
@@ -38,9 +44,36 @@ public class SmartBear_deleteOrder {
 
         for (WebElement name: allNames) {
                 Assert.assertFalse(name.getText().equals("Mark Smith"), "This TC failed, it has name in the list");
-
         }
+    }
 
+    @Test
+    public void editOrder() {
+        SmartBearMain.login(driver);
+        String editForSteveJohnsLocatorXpath = "//table[@id='ctl00_MainContent_orderGrid']/tbody/tr/td[.='Steve Johns']/../td[13]";
+        driver.findElement(By.xpath(editForSteveJohnsLocatorXpath)).click();
+        String customerNameFieldLocatorID = "ctl00_MainContent_fmwOrder_txtName";
+        driver.findElement(By.id(customerNameFieldLocatorID)).sendKeys("Michael Jordan");
+        String updateBtnLocatorID = "ctl00_MainContent_fmwOrder_UpdateButton";
+        driver.findElement(By.id(updateBtnLocatorID)).click();
 
+        List<WebElement> allNames = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']/tbody/tr/td[2]"));
+
+        for (WebElement name : allNames) {
+            System.out.println(name.getText());
+            if (name.getText().equals("Micahel Jordan")) {
+                Assert.assertTrue(name.getText().equals("Michael Jordan"), "This name is not in the list");
+            }
+        }
+    }
+
+    @Test
+    public void deleteName () {
+        SmartBearMain.deleteName(driver, "Bob Feather");
+    }
+
+    @AfterClass
+    public void exit () {
+        driver.quit();
     }
 }
