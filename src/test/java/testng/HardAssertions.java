36ed82b0a971e43f466258b9489365ae85440677
AntonSemenov87 <Anthony04061987@gmail.com>
2020-10-16 22:17:31 -0500

    soft asserts

---
+++ src/test/java/testng/HardAssertions.java
@@ -0,0 +1,40 @@
+package testng;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class HardAssertions {
+
+    @Test
+    public void test1() {
+        System.out.println("Test1 starting");
+        Assert.assertEquals(4, 5);
+
+        System.out.println("Test1 middle");
+        Assert.assertTrue(false);
+
+        System.out.println("Test1 ending");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test2 is starting");
+    }
+
+    @Test
+    public void test3(){
+        SoftAssert softAssert = new SoftAssert();
+
+        System.out.println("test3 is starting");
+        softAssert.assertEquals(4, 5);
+
+        System.out.println("test3 middle");
+        softAssert.assertTrue(false);
+
+        System.out.println("test3 ending");
+
+    }
+
+
+}
